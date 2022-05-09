273f6031315da0fb3421d8e1855526303882b87b
AliHakim773 <93867233+AliHakim773@users.noreply.github.com>
2022-05-09 11:38:21 +0300

    frontend

---
+++ frontend/components/constant/menu.js
@@ -0,0 +1,21 @@
+export const MENUITEMS = [
+  {
+    title: "Products",
+    type: "sub",
+    children: [
+      {
+        path: "/product-details/no-sidebar",
+        title: "No Sidebar",
+        type: "link",
+      },
+    ],
+  },
+  {
+    title: "Blogs",
+    type: "sub",
+    children: [
+      { path: "/blogs/no_sidebar", title: "no sidebar", type: "link" },
+      { path: "/blogs/blog_detail", title: "blog detail", type: "link" },
+    ],
+  },
+];
---
+++ frontend/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Beauty from "./layouts/Beauty/index";
+
+export default function index() {
+  return <Beauty />;
+}
